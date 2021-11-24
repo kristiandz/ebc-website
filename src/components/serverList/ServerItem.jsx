cedaad103377c0f791d168816be4ee78032e5491
Kristian Džidžić <wowisnikr@gmail.com>
2021-11-24 22:44:37 +0100

    Server items

--- src/components/serverList/ServerItem.jsx
+++ src/components/serverList/ServerItem.jsx
@@ -1,11 +1,18 @@
 import classes from "./ServerItem.module.css"
 
 function ServerItem() {
-    return <div className={classes.serveritem}>
-        <h2>Promod</h2>
-        <h2>Mixmod</h2>
-        <h2>CodJumper</h2>
-    </div>
+    return (
+        <div className={classes.serverItem}>
+            <h4>Server Title</h4>
+            <h5>Short server description</h5>
+            <span>Game: </span>
+            <span>Current map:</span>
+            <span>Players:</span>
+            <span>View screenshots..</span>
+            <span>Copy connect info to clipboard</span>
+            <span>Click for more information..</span>
+        </div>
+    )
 }
 
 export default ServerItem;
--- src/pages/Servers.jsx
+++ src/pages/Servers.jsx
@@ -3,8 +3,12 @@ import ServerItem from "../components/serverList/ServerItem";
 
 function Servers() {
     return <div className={classes.servers}>
-        <h1>Server List</h1>
-        <ServerItem />
+        <h3>Servers we are currently offering for you to enjoy</h3>
+        <hr />
+        <div className={classes.itemContainer}>
+            <ServerItem />
+            <ServerItem />
+        </div>
     </div>
 }
 
