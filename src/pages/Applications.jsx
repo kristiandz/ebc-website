b90547e85c52a8a079143957c94b882eb9138dd1
Kristian Džidžić <wowisnikr@gmail.com>
2021-12-02 19:38:23 +0100

    Applications page added with routing
    - Placeholder for application pages, like appeals, applications etc.
    - Using router v6
    
    <Outlet/> displays the component that is routed from app.js

---
+++ src/pages/Applications.jsx
@@ -0,0 +1,19 @@
+import classes from "./Applications.module.css";
+import {Outlet, Link} from "react-router-dom";
+
+function Applications() {
+
+    return (
+        <div className = {classes.appContainer}>
+            <nav>
+                <Link to="community_applications">Community applications</Link>
+                <Link to="community_apply">Join the community</Link>
+                <Link to="ban_appeals">Ban appeals</Link>
+                <Link to="ban_appeal">Appeal a ban</Link>
+            </nav>
+            <Outlet/>
+        </div>
+     );
+}   
+
+export default Applications;
