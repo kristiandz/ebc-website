1398590daadfbaa916dd8c06fd80dfef9b2b16ce
Kristian Džidžić <wowisnikr@gmail.com>
2021-12-02 21:26:43 +0100

    Structure update

--- src/pages/Servers.jsx
+++ src/pages/Servers.jsx
@@ -1,15 +1,18 @@
 import classes from "./Servers.module.css";
-import ServerItem from "../components/serverList/ServerItem";
+import ServerItem from "../components/serverItems/ServerItem";
 
 function Servers() {
-    return <div className={classes.servers}>
-        <h3>Servers we are currently offering for you to enjoy</h3>
-        <hr />
-        <div className={classes.itemContainer}>
-            <ServerItem />
-            <ServerItem />
-        </div>
+  return (
+    <div className={classes.servers}>
+      <h3>Servers we are currently offering for you to enjoy</h3>
+      <hr />
+      <div className={classes.itemContainer}>
+        <ServerItem />
+        <ServerItem />
+        <ServerItem />
+      </div>
     </div>
+  );
 }
 
 export default Servers;
