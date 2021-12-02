a4a7552db90314aedfce98affa5b422225fd5c5e
Kristian Džidžić <wowisnikr@gmail.com>
2021-12-02 19:34:38 +0100

    App routing updates, new components
    - Using router v6 now, changed the switch to routes
    - Added nested routing for secondary navigation bar for applications page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import { Route, Switch } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import Servers from "./pages/Servers";
 import Hosting from "./pages/Hosting";
+import Applications from "./pages/Applications";
+import CommunityApplications from "./components/applicationPages/CommunityApplications";
+import CommunityApply from "./components/applicationPages/CommunityApply";
+import BanAppeals from "./components/applicationPages/BanAppeals";
+import AppealBan from "./components/applicationPages/AppealBan";
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact={true}>
-          <Home />
+      <Routes>
+        <Route path="/" exact={true} element = {<Home/>} />
+        <Route path="/about" element = {<Home />} />
+        <Route path="/servers" element = {<Servers />} />
+        <Route path="/hosting" element = {<Hosting/>} />
+        <Route path="/applications" element = {<Applications/>}>
+          <Route path = "community_applications" element = {<CommunityApplications/>} />
+          <Route path = "community_apply" element = {<CommunityApply/>} />
+          <Route path = "ban_appeals" element = {<BanAppeals/>} />
+          <Route path = "ban_appeal" element = {<AppealBan/>} />
         </Route>
-        <Route path="/about">
-          <Home />
-        </Route>
-        <Route path="/servers">
-          <Servers />
-        </Route>
-        <Route path="/hosting">
-          <Hosting />
-        </Route>
-      </Switch>
+      </Routes>
     </Layout>
   );
 }
