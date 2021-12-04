3155c5b61851d5cb46e48bc63b794f8a31d3dcdf
Kristian Džidžić <wowisnikr@gmail.com>
2021-12-04 14:54:07 +0100

    ApplicationComment component
    - Used to display comments inside the application card

---
+++ src/components/applicationItems/ApplicationComment.jsx
@@ -0,0 +1,21 @@
+import classes from "./ApplicationComment.module.css";
+
+function ApplicationComment(props) {
+    return (
+        <div className={classes.container}>
+            <span className = {classes.comment}>"{props.comment}"</span>
+            <div className={classes.details}>
+                <div className={classes.cardRow}>
+                    <span className={classes.cardForm}>Discord Tag: </span>
+                    <span className={classes.cardData}>{props.tag}</span>
+                </div>
+                <div className={classes.cardRow}>
+                    <span className={classes.cardForm}>Time created: </span>
+                    <span className={classes.cardData}>{props.time}</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ApplicationComment;
