c29349c2e45caae47ac70ff0f62dec5f8ce44b32
Kristian Džidžić <kristian334@gmail.com>
2021-12-07 09:12:55 +0100

    Get correct comment values

--- src/components/applicationItems/ApplicationComment.jsx
+++ src/components/applicationItems/ApplicationComment.jsx
@@ -3,15 +3,15 @@ import classes from "./ApplicationComment.module.css";
 function ApplicationComment(props) {
     return (
         <div className={classes.container}>
-            <span className = {classes.comment}>"{props.comment}"</span>
+            <span className = {classes.comment}>"{props.value.comment}"</span>
             <div className={classes.details}>
                 <div className={classes.cardRow}>
                     <span className={classes.cardForm}>Discord Tag: </span>
-                    <span className={classes.cardData}>{props.tag}</span>
+                    <span className={classes.cardData}>{props.value.tag}</span>
                 </div>
                 <div className={classes.cardRow}>
                     <span className={classes.cardForm}>Time created: </span>
-                    <span className={classes.cardData}>{props.time}</span>
+                    <span className={classes.cardData}>{props.value.time}</span>
                 </div>
             </div>
         </div>
