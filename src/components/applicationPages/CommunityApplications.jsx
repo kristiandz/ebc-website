9a8b73a126fc130d1f32aa43a116724bc9bf74f7
Kristian Džidžić <kristian334@gmail.com>
2021-12-07 09:09:34 +0100

    Community Applications update
    - Fetching applications from a backend
    - Reversing the order to display the latest app first
    TODO: Separate active and inactive applications

--- src/components/applicationPages/CommunityApplications.jsx
+++ src/components/applicationPages/CommunityApplications.jsx
@@ -1,7 +1,34 @@
+import ApplicationCard from "../applicationItems/ApplicationCard";
+import classes from "./CommunityApplications.module.css";
+import {useState, useEffect} from "react";
+
 function CommunityApplications() {
+
+    const [applications,setApplications] = useState([]);
+
+    useEffect(() => {
+        const getApplications = async () => {
+            const applicationsFromServer = await fetchApplications();
+            setApplications(applicationsFromServer);
+        }
+        getApplications();
+    },[])
+
+    const fetchApplications = async () => {
+        const res = await fetch("http://localhost:3001/applications");
+        const data = await res.json();
+        return data;
+    }
+
+    const reversedApplications = applications.map(item => item).reverse(); // To display the latest application first
+
+    // Check for status and reder active/inactive applications
     return ( 
-        <div>
-            <h1>List of all applications</h1>
+        <div className = {classes.container}>
+            <h3>Active applications</h3>
+            {reversedApplications.map((element)=> {return(<ApplicationCard value={element} key={element.id}/>)})}
+            <h3>Archive</h3>
+            {reversedApplications.map((element)=> {return(<ApplicationCard value={element} key={element.id}/>)})}
         </div>
      );
 }
