681d79d024fc014c1fbb91b70521d6317fbf289d
Kristian Džidžić <wowisnikr@gmail.com>
2021-12-18 19:04:59 +0100

    Ban appeals page & Style
    Get all the ban appeal cards from the server and display them

--- src/components/applicationPages/BanAppeals.jsx
+++ src/components/applicationPages/BanAppeals.jsx
@@ -1,9 +1,35 @@
+import classes from "./BanAppeals.module.css";
+import { useState, useEffect } from "react";
+import AppealCard from "../applicationItems/AppealCard";
+
 function BanAppeals() {
-    return ( 
-        <div>
-            <h1>List of all ban appeals</h1>
+    const [banAppeals, setBanAppeals] = useState([]);
+
+    useEffect(() => {
+        const getAppeals = async () => {
+            const appealsFromServer = await fetchAppeals();
+            setBanAppeals(appealsFromServer);
+        }
+        getAppeals();
+    }, [])
+
+    const fetchAppeals = async () => {
+        const res = await fetch("http://localhost:3001/appeals");
+        const data = await res.json();
+        return data;
+    }
+
+    const reversedAppeals = banAppeals.map(item => item).reverse(); // To display the latest appeals first
+
+    // Check for status and render active/inactive appeals
+    return (
+        <div className={classes.container}>
+            <h3>Active appeals</h3>
+            {reversedAppeals.map((element) => { return (<AppealCard value={element} key={element.id} />) })}
+            <h3>Archive</h3>
+            {reversedAppeals.map((element) => { return (<AppealCard value={element} key={element.id} />) })}
         </div>
-     );
+    );
 }
 
 export default BanAppeals;
