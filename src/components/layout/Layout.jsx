a7dfa3a73d4a13a84cdd587f13cc8f30d779966f
Kristian Džidžić <wowisnikr@gmail.com>
2022-01-02 09:40:00 +0100

    Style update

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,6 +2,7 @@ import classes from "./Layout.module.css";
 import MainNavigation from "./MainNavigation";
 
 function Layout(props) {
+    
     return (
         <div>
             <MainNavigation />
