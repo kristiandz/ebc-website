19546fb137128882fcc25c41f412dae447d4aabe
Kristian Džidžić <wowisnikr@gmail.com>
2022-01-02 13:39:59 +0100

    Servers page implemented
    - Fetching data from the server and displaying info for each server

--- src/pages/Servers.jsx
+++ src/pages/Servers.jsx
@@ -1,15 +1,31 @@
 import classes from "./Servers.module.css";
 import ServerItem from "../components/serverItems/ServerItem";
+import { useState, useEffect } from "react";
 
 function Servers() {
+
+  const [servers, setServers] = useState([]);
+  
+  useEffect(() => {
+    const getServers = async () => {
+      const servers = await fetchServers();
+      setServers(servers);
+    }
+    getServers();
+  }, [])
+  
+  const fetchServers = async () => {
+    const res = await fetch("http://localhost:3001/servers");
+    const data = await res.json();
+    return data;
+  }
+
   return (
     <div className={classes.servers}>
       <h3>Servers we are currently offering for you to enjoy</h3>
       <hr />
       <div className={classes.itemContainer}>
-        <ServerItem />
-        <ServerItem />
-        <ServerItem />
+        {servers.map((element) => { return (<ServerItem value={element} key={element.id} />) })}
       </div>
     </div>
   );
