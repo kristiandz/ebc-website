70f275c1b275bd0563da6052e7185ff82c4fdb65
Kristian Džidžić <wowisnikr@gmail.com>
2022-01-02 13:39:28 +0100

    ServerItem component update

--- src/components/serverItems/ServerItem.jsx
+++ src/components/serverItems/ServerItem.jsx
@@ -1,16 +1,21 @@
 import classes from "./ServerItem.module.css"
 
-function ServerItem() {
+function ServerItem(props) {
+
     return (
         <div className={classes.serverItem}>
-            <h4>Server Title</h4>
-            <h5>Short server description</h5>
-            <span>Game: </span>
-            <span>Current map:</span>
-            <span>Players:</span>
-            <span>View screenshots..</span>
-            <span>Copy connect info to clipboard</span>
-            <span>Click for more information..</span>
+            <h4>{props.value.title}</h4>
+            <div className={classes.serverDetails}>
+                <span>{props.value.desc}</span>
+                <span>Game: {props.value.game}</span>
+                <span>Current map: {props.value.map}</span>
+                <span>Current players: {props.value.players}/{props.value.slots}</span>
+            </div>
+            <div className={classes.serverInfo}>
+                <button>View screenshots</button>
+                <button>Copy connect info</button>
+                <button>Join server</button>
+            </div>
         </div>
     )
 }
