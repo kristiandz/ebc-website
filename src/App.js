5ed3e45ff256815dc38ceca78b7a9c38ebeec3a5
Kristian Džidžić <kristian334@gmail.com>
2022-01-04 16:49:09 +0100

    Missing page added, warning fix

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import Servers from "./pages/Servers";
 import Hosting from "./pages/Hosting";
 import League from "./pages/League";
 import Stats from "./pages/Stats";
+import Panel from "./pages/Panel";
 import Applications from "./pages/Applications";
 import CommunityApplications from "./components/applicationPages/CommunityApplications";
 import CommunityApply from "./components/applicationPages/CommunityApply";
 import BanAppeals from "./components/applicationPages/BanAppeals";
 import AppealBan from "./components/applicationPages/AppealBan";
+// import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" exact={true} element = {<Home/>} />
-        <Route path="/about" element = {<Home />} />
-        <Route path="/servers" element = {<Servers />} />
-        <Route path="/hosting" element = {<Hosting/>} />
-        <Route path="/league" element = {<League/>} />
-        <Route path="/stats" element={<Stats/>} />
-        <Route path="/applications" element = {<Applications/>}>
-          <Route path = "community_applications" element = {<CommunityApplications/>} />
-          <Route path = "community_apply" element = {<CommunityApply/>} />
-          <Route path = "ban_appeals" element = {<BanAppeals/>} />
-          <Route path = "ban_appeal" element = {<AppealBan/>} />
+        {/* <Route path="*" component={<NotFound />} /> */}
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/about" element={<Home />} />
+        <Route path="/servers" element={<Servers />} />
+        <Route path="/hosting" element={<Hosting />} />
+        <Route path="/league" element={<League />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/applications" element={<Applications />}>
+          <Route path="community_applications" element={<CommunityApplications />} />
+          <Route path="community_apply" element={<CommunityApply />} />
+          <Route path="ban_appeals" element={<BanAppeals />} />
+          <Route path="ban_appeal" element={<AppealBan />} />
         </Route>
+        <Route path="/panel" element={<Panel />} />
       </Routes>
     </Layout>
   );
---
+++ src/pages/Panel.jsx
@@ -0,0 +1,11 @@
+import classes from "./Panel.module.css";
+
+function Panel() {
+    return (
+        <div className={classes.panel}>
+            <h2>Panel is rendered here</h2>
+        </div>
+    );
+}
+
+export default Panel;
