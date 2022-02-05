d5154b2d511e78bed202cbffcc0cb40343ea82b2
Kristian Džidžić <wowisnikr@gmail.com>
2022-02-05 14:44:09 +0100

    ServerItem component implemented

--- src/components/serverItems/ServerItem.jsx
+++ src/components/serverItems/ServerItem.jsx
@@ -1,22 +1,53 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import classes from "./ServerItem.module.css"
 
 function ServerItem(props) {
 
+    const [isShown, setIsShown] = useState(false);
+
+    function showPlayerList() {
+        setIsShown(!isShown);
+    }
+
+    let background = { background: "lime" };
+    if (props.value.status === "active")
+        background = { background: "lime" };
+    else if (props.value.status === "inactive")
+        background = { background: "red" };
+        
     return (
         <div className={classes.serverItem}>
-            <h4>{props.value.title}</h4>
+            <h4><span className={classes.status} style={background}></span> {props.value.title}</h4>
+            <hr />
             <div className={classes.serverDetails}>
-                <span>{props.value.desc}</span>
                 <span>Game: {props.value.game}</span>
-                <span>Current map: {props.value.map}</span>
+                <span>Gametype: {props.value.gametype} </span>
+                <span>Map: {props.value.map}</span>
                 <span>Current players: {props.value.players}/{props.value.slots}</span>
             </div>
-            <div className={classes.serverInfo}>
-                <button>View screenshots</button>
-                <button>Copy connect info</button>
-                <button>Join server</button>
+            <hr />
+            <div onClick={showPlayerList} className={classes.playerList}>
+                {!isShown ? "Click to show playerlist" :
+                    <>
+                        <div className={classes.attack}>
+                            <span className={classes.teamTitle}>Attack:</span>
+                            {props.value.attack.map((element) => { return (<span key={element?.id}>{element?.name}</span>)})}
+                        </div>
+                        <div className={classes.defence}>
+                            <span className={classes.teamTitle}>Defence:</span>
+                            {props.value.defence.map((element) => { return (<span key={element?.id}>{element?.name}</span>)})}
+                        </div>
+                    </>
+                }
             </div>
-        </div>
+            <hr />
+            <div className={classes.serverInfo}>
+                <button><Link to="#">View screenshots</Link></button>
+                <button><Link to="#">Copy connect info</Link></button>
+                <button><Link to="#">Join server</Link></button>
+            </div>
+        </div >
     )
 }
 
