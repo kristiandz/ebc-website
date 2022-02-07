feb8834c50ae20c9ff4f7d2414f55311b2256892
Kristian Džidžić <kristian334@gmail.com>
2022-02-07 16:37:44 +0100

    Undefined check
    - Don't crash if there is no value present
    - Minor css adjustments

--- src/components/serverItems/ServerItem.jsx
+++ src/components/serverItems/ServerItem.jsx
@@ -18,13 +18,13 @@ function ServerItem(props) {
         
     return (
         <div className={classes.serverItem}>
-            <h4><span className={classes.status} style={background}></span> {props.value.title}</h4>
+            <h4><span className={classes.status} style={background}></span> {props.value?.title}</h4>
             <hr />
             <div className={classes.serverDetails}>
-                <span>Game: {props.value.game}</span>
-                <span>Gametype: {props.value.gametype} </span>
-                <span>Map: {props.value.map}</span>
-                <span>Current players: {props.value.players}/{props.value.slots}</span>
+                <span>Game: {props.value?.game}</span>
+                <span>Gametype: {props.value?.gametype} </span>
+                <span>Map: {props.value?.map}</span>
+                <span>Current players: {props.value?.players}/{props.value?.slots}</span>
             </div>
             <hr />
             <div onClick={showPlayerList} className={classes.playerList}>
@@ -32,11 +32,11 @@ function ServerItem(props) {
                     <>
                         <div className={classes.attack}>
                             <span className={classes.teamTitle}>Attack:</span>
-                            {props.value.attack.map((element) => { return (<span key={element?.id}>{element?.name}</span>)})}
+                            {props.value.attack?.map((element) => { return (<span key={element?.id}>{element?.name}</span>)})}
                         </div>
                         <div className={classes.defence}>
                             <span className={classes.teamTitle}>Defence:</span>
-                            {props.value.defence.map((element) => { return (<span key={element?.id}>{element?.name}</span>)})}
+                            {props.value.defence?.map((element) => { return (<span key={element?.id}>{element?.name}</span>)})}
                         </div>
                     </>
                 }
